bfaad48b6dbd47d1a357a207b3ef93d371ad46c5
maqiie <99504010+maqiie@users.noreply.github.com>
2023-12-29 14:48:19 +0300

    working on home and navbar

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,13 +1,23 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import img from "../../assets/shopping.jpg"; // Adjust the path based on your project structure
+import "./Home.css";
 
 class Home extends Component {
-    render() {
-        return (
-            <div>
-                
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div className="home-container">
+        <img src={img} alt="Shopping" className="centered-image" />
+        <div class="center">
+          New Outwear <br /> collection
+        </div>
+        <button>
+          <a href="#">
+            <span>Button</span>
+          </a>
+        </button>
+      </div>
+    );
+  }
 }
 
 export default Home;
