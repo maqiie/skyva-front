93f3597767cab1f013ad6f2052ec3f56f53fcc96
maqiie <99504010+maqiie@users.noreply.github.com>
2024-01-16 16:10:40 +0300

    updated the home

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,51 +1,3 @@
-// import React, { Component } from "react";
-// import "./Home.css";
-// import img from "../../assets/shopping.jpg";
-// import image1 from "../../assets/image1.jpg";
-// import image2 from "../../assets/image2.jpg";
-// import image3 from "../../assets/image3.jpg";
-
-// class Home extends Component {
-//   render() {
-//     return (
-//       <div className="home-container">
-//         <div className="image-container">
-//           <img src={img} alt="Shopping" className="centered-image" />
-//           <div className="center">
-//             New Outwear <br /> Collection
-//           </div>
-//         </div>
-//         <section className="new">
-//           <h2>Newest Products</h2>
-//           <div className="card-container">
-//             <div className="card">
-//               <img src={image1} alt="Product 1" />
-//               <p>Product 1 Description</p>
-//             </div>
-//             <div className="card">
-//               <img src={image2} alt="Product 2" />
-//               <p>Product 2 Description</p>
-//             </div>
-//             <div className="card">
-//               <img src={image3} alt="Product 3" />
-//               <p>Product 3 Description</p>
-//             </div>
-//           </div>
-//         </section>
-
-//         <button className="cta-button">
-//           <a className="a1" href="#">
-//             <span>Shop Now</span>
-//           </a>
-//         </button>
-//       </div>
-//     );
-//   }
-// }
-
-// export default Home;
-
-// Home.js
 import React, { Component } from "react";
 import AOS from "aos";
 import "aos/dist/aos.css";
@@ -55,23 +7,31 @@ import image1 from "../../assets/image1.jpg";
 import image2 from "../../assets/image2.jpg";
 import image3 from "../../assets/image3.jpg";
 
-
 class Home extends Component {
   componentDidMount() {
     AOS.init();
   }
 
-
   render() {
     return (
-      <div className="home-container"  >
-        <div className="image-container"  data-aos="fade-up" data-aos-delay="0">
+      <div className="home-container">
+        <div className="image-container" data-aos="fade-up" data-aos-delay="0">
           <img src={img} alt="Shopping" className="centered-image" />
-          <div className="center animated" data-aos="fade-up" data-aos-delay="200">
+          <div
+            className="center animated"
+            data-aos="fade-up"
+            data-aos-delay="200"
+          >
             New Outwear <br /> Collection
+
+            {/* Button inside the same div */}
+            <div className="button-container" data-aos="fade-up" data-aos-delay="300">
+              <button className="custom-button">Shop Now</button>
+            </div>
           </div>
         </div>
-        <section className="new"   data-aos="fade-up" data-aos-delay="400">
+
+        <section className="new" data-aos="fade-up" data-aos-delay="400">
           <h2>Newest Products</h2>
           <div className="card-container">
             <div className="card1">
@@ -88,7 +48,11 @@ class Home extends Component {
             </div>
           </div>
         </section>
-        <section className="hero-section"   data-aos="fade-up" data-aos-delay="600">
+        <section
+          className="hero-section"
+          data-aos="fade-up"
+          data-aos-delay="600"
+        >
           <div className="card-grid">
             <a className="card" href="#">
               <div
@@ -143,11 +107,9 @@ class Home extends Component {
               </div>
             </a>
           </div>
-          
-          
         </section>
         <section className="hero-section">
-        <div className="maps"   data-aos="fade-up" data-aos-delay="800">
+          <div className="maps" data-aos="fade-up" data-aos-delay="800">
             <iframe
               src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d247.621694221964!2d39.22974799535693!3d-6.776292510759074!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x185c4ffd9443848f%3A0x177d30c63cc6adc6!2sC!5e0!3m2!1sen!2stz!4v1704468258544!5m2!1sen!2stz"
               width="1500"
@@ -159,12 +121,8 @@ class Home extends Component {
               referrerPolicy="no-referrer-when-downgrade"
             ></iframe>
           </div>
+          
         </section>
-        <button className="cta-button"  data-aos="fade-up" data-aos-delay="1000">
-          <a className="a1" href="#">
-            <span>Shop Now</span>
-          </a>
-        </button>
       </div>
     );
   }
--- src/components/Navbar/Login/Login.js
+++ src/components/Navbar/Login/Login.js
@@ -1,35 +1,34 @@
 import React, { Component } from 'react';
-import './Login.css';  // Import the CSS file
+import './Login.css';
 
 class Login extends Component {
     render() {
         return (
-            <div class="main">  	
-		<input type="checkbox" id="chk" aria-hidden="true"/>
+            <div className="main">
+                <input type="checkbox" id="chk" aria-hidden="true" />
 
-			<div class="login">
-				<form class="form">
-					<label for="chk" aria-hidden="true">Log in</label>
-					<input class="input" type="email" name="email" placeholder="Email" required=""/>
-					<input class="input" type="password" name="pswd" placeholder="Password" required=""/>
-					<button>Log in</button>
-				</form>
-			</div>
+                <div className="login">
+                    <form className="form">
+                        <label htmlFor="chk" aria-hidden="true">Log in</label>
+                        <input className="input" type="email" name="email" placeholder="Email" required="" />
+                        <input className="input" type="password" name="pswd" placeholder="Password" required="" />
+                        <button>Log in</button>
+                    </form>
+                </div>
 
-      <div class="register">
-				<form class="form">
-					<label for="chk" aria-hidden="true">Register</label>
-					<input class="input" type="text" name="txt" placeholder="Username" required=""/>
-					<input class="input" type="email" name="email" placeholder="Email" required=""/>
-					<input class="input" type="password" name="pswd" placeholder="Password" required=""/>
-					<button>Register</button>
-				</form>
-			</div>
-	</div>
-
-    
+                <div className="register">
+                    <form className="form">
+                        <label htmlFor="chk" aria-hidden="true">Register</label>
+                        <input className="input" type="text" name="txt" placeholder="Username" required="" />
+                        <input className="input" type="email" name="email" placeholder="Email" required="" />
+                        <input className="input" type="password" name="pswd" placeholder="Password" required="" />
+                        <button>Register</button>
+                    </form>
+                </div>
+            </div>
         );
     }
 }
 
 export default Login;
+
