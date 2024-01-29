6874e72f132ee1ba449ed45842b94be37d15b5e8
maqiie <99504010+maqiie@users.noreply.github.com>
2024-01-29 18:33:40 +0300

    updated the login  and sign in part

--- src/components/Navbar/Login/Login.js
+++ src/components/Navbar/Login/Login.js
@@ -1,39 +1,97 @@
-
-
-import React, { Component } from 'react';
-import './Login.css';
+import React, { Component } from "react";
+import axios from "axios";
+import { toast, ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import "./Login.css";
 
 class Login extends Component {
   constructor(props) {
     super(props);
     this.state = {
       isSignUp: false,
-      usernameOrEmail: '',
-      password: '',
-      confirmPassword: '',
+      usernameOrEmail: "",
+      password: "",
+      confirmPassword: "",
+      dataFromServer: null,
     };
   }
 
+  // Toggle between login and signup
   handleSwitchClick = () => {
     this.setState((prevState) => ({
       isSignUp: !prevState.isSignUp,
     }));
   };
 
+  // Handle input changes for controlled components
   handleInputChange = (e) => {
     this.setState({
       [e.target.name]: e.target.value,
     });
   };
 
+  // Make a login request
+  loginRequest = async () => {
+    try {
+      const { usernameOrEmail, password } = this.state;
+      const response = await axios.post("http://localhost:3001/auth/signin", {
+        usernameOrEmail,
+        password,
+      });
+
+      // Update state with the server response
+      this.setState({
+        dataFromServer: response.data,
+      });
+
+      // Show success notification
+      toast.success("Login successful!");
+
+      console.log("Data from server:", response.data);
+    } catch (error) {
+      console.error("Error with login request:", error);
+      // Show error notification
+      toast.error("Login failed. Please check your credentials.");
+    }
+  };
+
+  // Make a registration request
+  registerRequest = async () => {
+    try {
+      const { usernameOrEmail, password, confirmPassword } = this.state;
+      const response = await axios.post("http://localhost:3001/auth", {
+        user: {
+          name: usernameOrEmail,
+          email: usernameOrEmail,
+          password,
+          password_confirmation: confirmPassword,
+        },
+      });
+
+      // Update state with the server response
+      this.setState({
+        dataFromServer: response.data,
+      });
+
+      // Show success notification
+      toast.success("User created successfully!");
+
+      console.log("Data from server:", response.data);
+    } catch (error) {
+      console.error("Error with registration request:", error);
+      // Show error notification
+      toast.error("Registration failed. Please try again.");
+    }
+  };
+
   render() {
-    const { isSignUp, usernameOrEmail, password, confirmPassword } = this.state;
+    const { isSignUp } = this.state;
 
     return (
       <div className="container1">
         <div className="card2">
           <a className="switch" onClick={this.handleSwitchClick}>
-            Switch to {isSignUp ? 'Login' : 'Sign Up'}
+            Switch to {isSignUp ? "Login" : "Sign Up"}
           </a>
           <div className="inputBox1">
             <input
@@ -42,7 +100,29 @@ class Login extends Component {
               name="usernameOrEmail"
               onChange={this.handleInputChange}
             />
-            <span className="user">{isSignUp ? 'Email' : 'Username'}</span>
+            <span className="user">{isSignUp ? "Email" : "Username"}</span>
+          </div>
+
+          {isSignUp && (
+            <div className="inputBox">
+              <input
+                type="text"
+                required="required"
+                name="name"
+                onChange={this.handleInputChange}
+              />
+              <span>Name</span>
+            </div>
+          )}
+
+          <div className="inputBox">
+            <input
+              type="password"
+              required="required"
+              name="password"
+              onChange={this.handleInputChange}
+            />
+            <span>Password</span>
           </div>
 
           {isSignUp && (
@@ -50,41 +130,22 @@ class Login extends Component {
               <input
                 type="password"
                 required="required"
-                name="password"
-                value={password}
-                onChange={this.handleInputChange}
-              />
-              <span>Password</span>
-            </div>
-          )}
-
-          {isSignUp && (
-            <div className="inputBox">
-              <input
-                type="password"
-                required="required"
                 name="confirmPassword"
-                value={confirmPassword}
                 onChange={this.handleInputChange}
               />
               <span>Confirm Password</span>
             </div>
           )}
 
-          {!isSignUp && (
-            <div className="inputBox">
-              <input
-                type="password"
-                required="required"
-                name="password"
-                value={password}
-                onChange={this.handleInputChange}
-              />
-              <span>Password</span>
-            </div>
-          )}
+          <button
+            className="enter"
+            onClick={isSignUp ? this.registerRequest : this.loginRequest}
+          >
+            {isSignUp ? "Sign Up" : "Login"}
+          </button>
 
-          <button className="enter">{isSignUp ? 'Sign Up' : 'Login'}</button>
+          {/* Toast Container for displaying notifications */}
+          <ToastContainer />
         </div>
       </div>
     );
@@ -92,3 +153,4 @@ class Login extends Component {
 }
 
 export default Login;
+
