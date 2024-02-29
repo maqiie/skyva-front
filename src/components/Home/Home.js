72f3baf1f2ff861e40eee73198518e00f034800d
maqiie <99504010+maqiie@users.noreply.github.com>
2024-02-29 20:46:36 +0300

    updated the new products

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -68,35 +68,23 @@ const Home = () => {
     }
   };
 
-  // const fetchOnOfferProducts = async () => {
-  //   try {
-  //     const response = await axios.get(
-  //       "http://localhost:3001/products/on_offer"
-  //     );
-  //     // console.log("Fetched on offer products:", response.data);
-  //     setOnOfferProducts(response.data);
-  //     console.log("Cart ID in response data:", response.data.cartId);
-  //   } catch (error) {
-  //     console.error("Error fetching on offer products:", error);
-  //   }
-  // };
   const fetchOnOfferProducts = async () => {
     try {
       const response = await axios.get(
         "http://localhost:3001/products/on_offer"
       );
       // Log the fetched product data
-      console.log("Fetched on offer products:", response.data);
+      console.log("Fetched on offer products:");
 
       // Set the state with the fetched product data
       setOnOfferProducts(response.data);
 
       // Log the image URLs
       response.data.forEach((product) => {
-        console.log("Image URL for product:", product.image_url);
+        console.log("Image URL for product:");
       });
 
-      console.log("Cart ID in response data:", response.data.cartId);
+      console.log("Cart ID in response data:");
     } catch (error) {
       console.error("Error fetching on offer products:", error);
     }
@@ -232,56 +220,59 @@ const Home = () => {
 
       {/* <======newests products========> */}
 
-     
-<section className="new" data-aos="fade-up" data-aos-delay="400">
-  <h2 className="text-3xl text-black font-bold mb-8 text-center">
-    Newest Products
-  </h2>
+      <section className="new" data-aos="fade-up" data-aos-delay="400">
+        <h2 className="text-3xl text-black font-bold mb-8 text-center">
+          Newest Products
+        </h2>
 
-  <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-8">
-    {recentProducts.slice(0, 8).map((product, index) => (
-      <div key={index} className="card1 bg-white p-4 rounded-md shadow-md">
-        <div className="w-full h-60 overflow-hidden mb-4 rounded-md">
-          {product.image_url && (
-            <img
-              src={`http://localhost:3001${product.image_url}`}
-              alt={product.name}
-              className="w-full h-full object-cover rounded-md"
-              style={{ aspectRatio: '16/10' }} // Adjust the aspect ratio as needed
-            />
-          )}
-        </div>
-
-        <div className="flex justify-between items-center mb-2">
-          <p className="text-lg font-semibold text-black">{product.name}</p>
-          <p className="text-lg font-semibold text-black">{product.price} TZS</p>
-          {/* <p className="text-lg font-semibold text-black"{product.description}></p> */}
-        </div>
-
-        <button
-          className="CartBtn mt-4"
-          onClick={() => addToCart(product.id)}
-        >
-          <span className="IconContainer">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              height="1em"
-              viewBox="0 0 576 512"
-              fill="rgb(17, 17, 17)"
-              className="cart"
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-8">
+          {recentProducts.slice(0, 8).map((product, index) => (
+            <div
+              key={index}
+              className="card1 bg-white p-4 rounded-md shadow-md"
             >
-              <path d="M0 24C0 10.7 10.7 0 24 0H69.5c22 0 41.5 12.8 50.6 32h411c26.3 0 45.5 25 38.6 50.4l-41 152.3c-8.5 31.4-37 53.3-69.5 53.3H170.7l5.4 28.5c2.2 11.3 12.1 19.5 23.6 19.5H488c13.3 0 24 10.7 24 24s-10.7 24-24 24H199.7c-34.6 0-64.3-24.6-70.7-58.5L77.4 54.5c-.7-3.8-4-6.5-7.9-6.5H24C10.7 48 0 37.3 0 24zM128 464a48 48 0 1 1 96 0 48 48 0 1 1 -96 0zm336-48a48 48 0 1 1 96 0 48 48 0 1 1 -96 0z"></path>
-            </svg>
-          </span>
-          <span className="ml-2">Add to Cart</span>
-        </button>
-      </div>
-    ))}
-  </div>
-</section>
+              <div className="w-full h-60 overflow-hidden mb-4 rounded-md">
+                {product.image_url && (
+                  <img
+                    src={`http://localhost:3001${product.image_url}`}
+                    alt={product.name}
+                    className="w-full h-full object-cover rounded-md"
+                    style={{ aspectRatio: "16/10" }} // Adjust the aspect ratio as needed
+                  />
+                )}
+              </div>
 
+              <div className="flex justify-between items-center mb-2">
+                <p className="text-lg font-semibold text-black">
+                  {product.name}
+                </p>
+                <p className="text-lg font-semibold text-black">
+                  {product.price} TZS
+                </p>
+                {/* <p className="text-lg font-semibold text-black"{product.description}></p> */}
+              </div>
 
-
+              <button
+                className="CartBtn mt-4"
+                onClick={() => addToCart(product.id)}
+              >
+                <span className="IconContainer">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    height="1em"
+                    viewBox="0 0 576 512"
+                    fill="rgb(17, 17, 17)"
+                    className="cart"
+                  >
+                    <path d="M0 24C0 10.7 10.7 0 24 0H69.5c22 0 41.5 12.8 50.6 32h411c26.3 0 45.5 25 38.6 50.4l-41 152.3c-8.5 31.4-37 53.3-69.5 53.3H170.7l5.4 28.5c2.2 11.3 12.1 19.5 23.6 19.5H488c13.3 0 24 10.7 24 24s-10.7 24-24 24H199.7c-34.6 0-64.3-24.6-70.7-58.5L77.4 54.5c-.7-3.8-4-6.5-7.9-6.5H24C10.7 48 0 37.3 0 24zM128 464a48 48 0 1 1 96 0 48 48 0 1 1 -96 0zm336-48a48 48 0 1 1 96 0 48 48 0 1 1 -96 0z"></path>
+                  </svg>
+                </span>
+                <span className="ml-2">Add to Cart</span>
+              </button>
+            </div>
+          ))}
+        </div>
+      </section>
 
       {/* <=========slider images========> */}
       <div className="background-slider">
@@ -299,7 +290,7 @@ const Home = () => {
       </div>
 
       {/* <=======offer========> */}
-      <section className="py-8">
+      {/* <section className="py-8">
         <div className="text-center p-10">
           <h1 className="font-bold text-4xl mb-4 text-cyan-300">ON OFFER</h1>
         </div>
@@ -390,7 +381,102 @@ const Home = () => {
             ))}
           </section>
         </section>
-      </section>
+      </section> */}
+    <section className="py-8">
+  <div className="text-center p-10">
+    <h1 className="font-bold text-4xl mb-4 text-cyan-300">ON OFFER</h1>
+  </div>
+
+  
+    <section
+      id="Projects"
+      className="w-fit mx-auto grid grid-cols-1 lg:grid-cols-3 md:grid-cols-2 justify-items-center justify-center gap-y-20 gap-x-14 mt-10 mb-5 "
+    >
+      {onOfferProducts.slice(0, 9).map((product) => (
+        <div
+          key={product.id}
+          className="w-72 bg-white shadow-md rounded-xl duration-500 hover:scale-105 hover:shadow-xl"
+        >
+          <a href="#">
+            <img
+              src={`http://localhost:3001${product.image_url}`}
+              alt={product.name}
+              className="h-80 w-72 object-cover rounded-t-xl"
+              onError={(e) => {
+                console.error("Error loading image:", e);
+              }}
+            />
+
+            <div className="px-4 py-3 w-72">
+              <span className="text-gray-400 mr-3 uppercase text-xs">
+                Brand: {product.brand}
+              </span>
+              <p className="text-lg font-bold text-black truncate block">
+                Size: {product.size}
+              </p>
+              <p className="text-base font-medium text-gray-500 capitalize">
+                Category: {product.category}
+              </p>
+              <p className="text-lg font-bold text-black truncate block capitalize">
+                Name: {product.name}
+              </p>
+              <p className="text-sm text-gray-600">
+                Description: {product.description}
+              </p>
+              <div className="flex items-center">
+                <p className="text-lg font-semibold text-black cursor-auto my-3">
+                  Price: {product.price}
+                </p>
+                {product.previousPrice && (
+                  <del>
+                    <p className="text-sm text-gray-600 cursor-auto ml-2">
+                      Previous Price: {product.previousPrice}
+                    </p>
+                  </del>
+                )}
+                <div className="ml-auto">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="20"
+                    height="20"
+                    fill="currentColor"
+                    className="bi bi-bag-plus"
+                    viewBox="0 0 16 16"
+                  >
+                    <path
+                      fillRule="evenodd"
+                      d="M8 7.5a.5.5 0 0 1 .5.5v1.5H10a.5.5 0 0 1 0 1H8.5V12a.5.5 0 0 1-1 0v-1.5H6a.5.5 0 0 1 0-1h1.5V8a.5.5 0 0 1 .5-.5z"
+                    />
+                    <path d="M8 1a2.5 2.5 0 0 1 2.5 2.5V4h-5v-.5A2.5 2.5 0 0 1 8 1zm3.5 3v-.5a3.5 3.5 0 1 0-7 0V4H1v10a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V4h-3.5zM2 5h12v9a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1V5z" />
+                  </svg>
+
+                  <button
+                    className="CartBtn mt-4"
+                    onClick={() => addToCart(product.id)}
+                  >
+                    <span className="IconContainer">
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        height="1em"
+                        viewBox="0 0 576 512"
+                        fill="rgb(17, 17, 17)"
+                        className="cart"
+                      >
+                        <path d="M0 24C0 10.7 10.7 0 24 0H69.5c22 0 41.5 12.8 50.6 32h411c26.3 0 45.5 25 38.6 50.4l-41 152.3c-8.5 31.4-37 53.3-69.5 53.3H170.7l5.4 28.5c2.2 11.3 12.1 19.5 23.6 19.5H488c13.3 0 24 10.7 24 24s-10.7 24-24 24H199.7c-34.6 0-64.3-24.6-70.7-58.5L77.4 54.5c-.7-3.8-4-6.5-7.9-6.5H24C10.7 48 0 37.3 0 24zM128 464a48 48 0 1 1 96 0 48 48 0 1 1 -96 0zm336-48a48 48 0 1 1 96 0 48 48 0 1 1 -96 0z"></path>
+                      </svg>
+                    </span>
+                    <span className="ml-2">Add to Cart</span>
+                  </button>
+                </div>
+              </div>
+            </div>
+          </a>
+        </div>
+      ))}
+    </section>
+
+</section>
+
 
       <section className="hero-section">
         <div className="maps" data-aos="fade-up" data-aos-delay="800">
