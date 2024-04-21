ee2226ae50f11b9acec0b00d146913ff40da0be3
maqiie <markpaulmuthauragmail.com>
2024-04-21 14:29:54 +0300

    made the navbar sticky

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -66,7 +66,7 @@ const Navbar = () => {
   };
 
   return (
-    <nav className="bg-gray-800 p-4">
+    <nav className="bg-gray-800 p-4 sticky top-0 z-50">
       <div className="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8">
         <div className="relative flex items-center justify-between h-16">
           <div className="flex items-center justify-between w-full">
