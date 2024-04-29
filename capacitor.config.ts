40f67a495bb39b53bb652e1cf4836f61518a767e
maqiie <markpaulmuthauragmail.com>
2024-04-29 19:51:45 +0300

    Track large files with Git LFS

--- capacitor.config.ts
+++
@@ -1,9 +0,0 @@
-import type { CapacitorConfig } from '@capacitor/cli';
-
-const config: CapacitorConfig = {
-  appId: 'skyva.com',
-  appName: 'skva-front',
-  webDir: 'build'
-};
-
-export default config;
