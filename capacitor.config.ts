27f0be6fc7eefd136616aa09899b2f5009f79e2f
maqiie <markpaulmuthauragmail.com>
2024-04-29 18:13:04 +0300

    ionic capacitors1

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,8 +1,8 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'com.example.app',
-  appName: 'skyva-front',
+  appId: 'skyva.com',
+  appName: 'skva-front',
   webDir: 'build'
 };
 
